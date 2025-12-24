a83d20d784f1f2df42583e83996a5c8be421b353
Ava9597 <bonobonoyah1997@gmail.com>
2025-12-24 15:01:59 +0530

    soled the errors

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
-const { app, BrowserWindow } = require("electron");
-const path = require("path");
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
-function createWindow(){
-    const win = new BrowserWindow({
-        window: 1000,
-        height: 700,
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 700,
+        height: 600,
         webPreferences: {
-            preload: path.join(__dirname, "prelosad.js")
+            nodeIntegration: false,   
+            contextIsolation: true
         }
     });
-    win.loadFile("index.html")
+
+    mainWindow.loadFile(path.join(__dirname, 'index.html'));
 }
-app.whenReady().then(createWindow);
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
